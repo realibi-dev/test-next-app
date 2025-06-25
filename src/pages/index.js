32b86a2136d6f34938dc55506fead5d146ea7776
Alibi Duisenaliyev <108901007+realibi-dev@users.noreply.github.com>
2025-06-25 22:05:09 +0500

    text changed in main page

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+export default function Home() {
+  return (
+    <div style={{
+      width: "100%",
+      height: "90vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center"
+    }}>
+      <span style={{
+        fontSize: 30,
+        fontWeight: "bold",
+      }}>
+        Hello world, this is Alibi's website!
+      </span>
+    </div>
+  );
+}
